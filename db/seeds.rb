36da8b912c307305c0d80eee7a45b685e8087485
Paul McKissock <paulsmckissock@gmail.com>
2024-06-07 14:23:52 -0400

    deleted more unnecessary files. Replaced <br> with <div> in new.html.erb. Added seed data for articles.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+articles = [
+  { title: "Testing Rails Applications", link: "https://guides.rubyonrails.org/testing.html" },
+  { title: "Ruby on Rails Guides (v7.1.3.4)", link: "https://guides.rubyonrails.org/index.html" },
+  { title: "Getting Started with Rails", link: "https://guides.rubyonrails.org/getting_started.html" },
+  { title: "Active Record Basics", link: "https://guides.rubyonrails.org/active_record_basics.html" }
+]
+
+articles.each do |article_params|
+  Article.find_or_create_by!(title: article_params[:title]) do |article|
+    article.link = article_params[:link]
+  end
+end
