64c76e541e53f6715309350e2b5ece4fc252e522
Paul McKissock <paulsmckissock@gmail.com>
2024-06-10 11:36:13 -0400

    added articles description

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,10 @@
+# Each article has a title and a link, both of which are required fields.
+# Will eventually add an author, score, and number-of-comments attributes.
+#
+# Validations:
+# - title: must be present
+# - link: must be present
+
 class Article < ApplicationRecord
   validates :title, presence: true
   validates :link, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -12,8 +12,8 @@
 
 ActiveRecord::Schema[7.1].define(version: 2024_06_06_181615) do
   create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "link"
+    t.string "title", null: false
+    t.text "link", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
