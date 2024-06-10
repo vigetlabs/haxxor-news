13bc1acffcf5c0e03680dc90e3a15ecf949b3b57
Paul McKissock <paulsmckissock@gmail.com>
2024-06-10 11:21:26 -0400

    disallow null in database

--- db/migrate/20240606181615_create_articles.rb
+++ db/migrate/20240606181615_create_articles.rb
@@ -1,8 +1,8 @@
 class CreateArticles < ActiveRecord::Migration[7.1]
   def change
     create_table :articles do |t|
-      t.string :title
-      t.text :link
+      t.string :title, null: false
+      t.text :link, null: false
 
       t.timestamps
     end
