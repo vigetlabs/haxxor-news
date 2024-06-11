6fd339f82e16893e176b53501c1db867724c9fbf
Paul McKissock <paulsmckissock@gmail.com>
2024-06-11 16:36:45 -0400

    Created users factory. Modified Articles factory so that it's associated with a user. Added and removed some tests, articles_controller_test still needs work.

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -2,7 +2,9 @@ require "test_helper"
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @article = FactoryBot.create(:article)
+    @user = FactoryBot.create(:user)
+    @article = FactoryBot.create(:article, user: @user)
+    @other_user = FactoryBot.create(:user, email: "other@example.com")
   end
 
   test "should get index" do
@@ -11,16 +13,24 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
     assert_not_nil assigns(:articles)
   end
 
-  test "should get new" do
+  test "should redirect new when not logged in" do
     get new_article_url
-    assert_response :success
+    assert_redirected_to login_url
   end
 
-  test "should create article" do
-    assert_difference("Article.count") do
-      post articles_url, params: {article: {title: "Hello Rails", link: "http://rails.com"}}
+  test "should create article if logged in" do
+    log_in_as(@user)
+    assert_difference('Article.count', 1) do
+      post articles_url, params: { article: { title: "New Article", link: "https://newlink.com" } }
     end
     assert_redirected_to article_path(Article.last)
+  end
+
+  test "should not create article if not logged in" do
+    assert_no_difference('Article.count') do
+      post articles_url, params: { article: { title: "New Article", link: "https://newlink.com" } }
+    end
+    assert_redirected_to login_url
   end
 
   test "should show article" do
@@ -28,10 +38,10 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should not create article with invalid params" do
-    assert_no_difference("Article.count") do
-      post articles_url, params: {article: {title: "", link: ""}}
-    end
-    assert_response :unprocessable_entity
+  private
+
+  # Simulate a logged-in user
+  def log_in_as(user)
+    post login_url, params: { session: { email: user.email, password: 'password' } }
   end
 end
