41e84e50815b7b4e38fc80c2c3e5a26810dc4ea9
Paul McKissock <paulsmckissock@gmail.com>
2024-06-13 16:55:36 -0400

    Created change password screen and button. Made a link to show current user in application.html.erb. Added edit_email.html.erb since I forgot to add in the last commit.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   has_many :articles
 
   validates :name, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
+  validates :email, presence: true, uniqueness: true, format: {with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address"}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
-  get "/users/:id/edit_email", to: "users#edit_email", as: "edit_user_email"
+
+  get "/users/:id/edit_email", to: "users#edit_email", as: "edit_email_user"
   patch "/users/:id/update_email", to: "users#update_email", as: "update_email_user"
-  
+  get "/users/:id/edit_password", to: "users#edit_password", as: "edit_password_user"
+  patch "/users/:id/update_password", to: "users#update_password", as: "update_password_user"
 end
