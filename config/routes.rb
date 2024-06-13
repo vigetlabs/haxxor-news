e9fc3849f1fc3d2294414fc1f9cb49d2e0d3af2a
Paul McKissock <paulsmckissock@gmail.com>
2024-06-13 16:39:50 -0400

    Made change email button and page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   root "articles#index"
   resources :articles
-  resources :users, only: [:create, :show]
+  resources :users
   get "/signup", to: "users#new"
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/users/:id/edit_email", to: "users#edit_email", as: "edit_user_email"
+  patch "/users/:id/update_email", to: "users#update_email", as: "update_email_user"
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_13_193343) do
     t.text "link", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
