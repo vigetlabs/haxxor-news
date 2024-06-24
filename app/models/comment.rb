faed24360c6ae18f9bd2c8a7bd44d18b1094aaef
Paul McKissock <paulsmckissock@gmail.com>
2024-06-24 12:07:53 -0400

    [#9] Made all replies show in a comments show page

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :article
   belongs_to :user
   validates :text, presence: true
-  
-  belongs_to :parent, class_name: 'Comment', optional: true
-  has_many :replies, class_name: 'Comment', foreign_key: :parent_id, dependent: :destroy
+
+  belongs_to :parent, class_name: "Comment", optional: true
+  has_many :replies, class_name: "Comment", foreign_key: :parent_id, dependent: :destroy
+  scope :without_parent, -> { where(parent_id: nil) }
 end
