5cf0f3a9e39a1117a3951a901a81b8802ee51efb
Paul McKissock <paulsmckissock@gmail.com>
2024-07-01 14:25:17 -0400

    [#27] Adjusted Header. Added more CSS.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_26_175659) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_01_165702) do
   create_table "articles", force: :cascade do |t|
     t.string "title", null: false
     t.text "link", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id", null: false
+    t.integer "comments_count"
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
