a89eb9a6258374a1d4937509aad806961554a4f1
Paul McKissock <paulsmckissock@gmail.com>
2024-07-02 16:37:20 -0400

    Changed article show and render_comment_tree so that it fits with new application.html.erb.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-  before_action :authorized, only: [:create]
+  before_action :authorized, only: [:create, :upvote, :downvote]
   helper_method :comment
 
   def show
