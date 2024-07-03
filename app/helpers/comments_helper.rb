2810d31c5a6b20da463a35254c0725baf45197b3
Paul McKissock <paulsmckissock@gmail.com>
2024-07-03 12:07:42 -0400

    Made User page work with application.html.

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -6,6 +6,13 @@ module CommentsHelper
                safe_concat button_to("Upvote", upvote_article_comment_path(comment.article, comment), method: :post, class: "btn btn-sm upvote-button", data: {votable_type: "Comment", votable_id: comment.id, article_id: comment.article.id})
                safe_concat button_to("Downvote", downvote_article_comment_path(comment.article, comment), method: :post, class: "btn btn-sm downvote-button", data: {votable_type: "Comment", votable_id: comment.id, article_id: comment.article.id})
 
+               #  concat button_to(upvote_article_comment_path(comment), method: :post, class: "btn btn-sm upvote-button", data: {votable_type: "Comment", votable_id: comment.id, article_id: comment.article.id}) do
+               #    content_tag(:i, "", class: "fas fa-arrow-up")
+               #  end
+               #  concat button_to(downvote_article_comment_path(comment), method: :post, class: "btn btn-sm downvote-button", data: {votable_type: "Comment", votable_id: comment.id, article_id: comment.article.id}) do
+               #    content_tag(:i, "", class: "fas fa-arrow-down")
+               #  end
+
                safe_concat content_tag(:span, "#{comment.score} points", id: "score-Comment-#{comment.id}", class: "score")
                concat raw("&nbsp;")
                safe_concat "by"
